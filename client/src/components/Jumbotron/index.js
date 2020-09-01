d20c52eb7571dd04875d15d9d5cb9acf29266e42
Scott Harris <scottsharris@yahoo.com>
2020-08-31 21:19:12 -0700

    completed desktop jumbotron

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,14 +1,36 @@
-import React from "react";
+import React, { Component } from 'react';
+import logo from '../../img/logo.png'
+import './style.css';
 
-function Jumbotron({ children }) {
-  return (
-    <div
-      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-      className="jumbotron"
-    >
-      {children}
-    </div>
-  );
+class Jumbotron extends Component {
+
+  state = {
+    counter: "18,287"
+  }
+
+  render() {
+    return (
+        <div class="row" id="jumbotron">
+          <div class="col-6">
+            <img id="jumbo-logo" src={logo} />
+          </div>
+          <div class="col-6">
+            <div class="row">
+              <h1 id="jumbo-text">Coffee lovers served...</h1>
+              <h1 id="jumbo-counter">{this.state.counter}</h1>
+            </div>
+            <div class="row">
+              <div class="col-2">
+                <button id="login-btn" class="btn button">Login</button>
+              </div>
+              <div class="col-2">
+                <button id="signup-btn" class="btn button">Sign Up</button>
+              </div>
+            </div>
+          </div>
+        </div>
+    );
+  }
 }
 
 export default Jumbotron;
