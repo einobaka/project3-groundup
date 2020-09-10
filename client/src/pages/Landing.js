ce2318481afbc5392bd02b500c2efdf9ede6198d
Scott Harris <scottsharris@yahoo.com>
2020-09-09 20:53:40 -0700

    changed background image on search page so it's seamless

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -26,7 +26,7 @@ class Landing extends Component {
             signUpSubmit={this.signUpSubmit}
             />
             <LoginModal 
-            loginSumbit={this.loginSubmit}
+            loginSubmit={this.loginSubmit}
             />
             <Jumbotron />
           </div>
