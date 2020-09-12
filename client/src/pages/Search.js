d985fb390f29a5a258a04ca969584e77fe74a433
einobaka <61556425+einobaka@users.noreply.github.com>
2020-09-11 20:55:01 -0700

    corrected a merge error on search

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -1,45 +1,37 @@
-import React, { Component }  from "react";
+import React, { Component } from "react";
 // import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Wrapper from "../components/Wrapper"
 import Nav from "../components/Nav";
-
 import API from '../utils/API';
-
 import SearchContainer from "../components/SearchContainer"
 import SignUpModal from '../components/SignUpModal';
 import LoginModal from '../components/LoginModal';
-
-
 
 class Search extends Component {
   signUpSubmit = data => {
     // console.log(data);
     API.signUpSubmit(data);
   }
-
+  render() {
+    return (
+      <div>
+        <div class="col-12">
+          <div class="row">
+            <SignUpModal
+              signUpSubmit={this.signUpSubmit}
+            />
+            <LoginModal
+              loginSubmit={this.loginSubmit}
+            />
+            <Wrapper>
+              <Nav />
+              <SearchContainer />
+            </Wrapper>
+          </div>
+        </div>
+      </div>
+    )
   }
-
-  
-      render() {
-        return (
-          <div>
-            <div class="col-12">
-              <div class="row">
-               <SignUpModal 
-                signUpSubmit={this.signUpSubmit}
-                />
-                <LoginModal 
-                loginSubmit={this.loginSubmit}
-                />
-                <Wrapper>
-                <Nav />
-                <SearchContainer />
-                </Wrapper>
-            </div>
-          </div>
-          </div>
-        )
-      }
-    }
+}
 
 export default Search;
