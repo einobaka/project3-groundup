fba66dff2f797e1d1ab133518db3ade41d69345f
Sumnima B <sumnimabhandari@gmail.com>
2020-09-11 20:38:08 -0700

    Updated models

--- models/drink.js
+++ models/drink.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const drinkSchema = new Schema({
-  
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "user",
+    required: true,
+  },
   name: { type: String, required: true },
   price: { type: Number, required: true },
   category: { type: String },
