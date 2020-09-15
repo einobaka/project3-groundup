7858a8283823f08c4ca1cf2b71256272b456693b
einobaka <61556425+einobaka@users.noreply.github.com>
2020-09-15 16:51:58 -0700

    updated search bar, filters, login button

--- client/src/components/DrinkList/index.js
+++ client/src/components/DrinkList/index.js
@@ -10,18 +10,19 @@ function ListItems(props) {
         <span><img alt="logo" src={props.logo} className="img-fluid" class="logo" /></span>
         <card>
           <div class="drink-type">
-            <h1 class="category">{props.category}</h1>
+            <h1 class="category">{props.name}</h1>
           </div>
           <div class="drink-price">
-            <p class="price"> ${props.price}</p>
+            {/* <p class="price"> <h5>${props.price}</h5></p> */}
           </div>
           <br></br>
           <div class="drink-details">
-            <p class="name"> {props.name} | </p>
-            <p class="size"> {props.size}oz  </p>
+            <p class="name"> {props.category} | </p>
+            <p class="size"> <h7>{props.size} oz</h7> </p>
+            <p class="price"> <h2> ${props.price} </h2></p>
           </div>
           <div class="drink-ratings">
-            <p> <StaticRating rating={props.rating} /></p>
+            <p><StaticRating rating={props.rating} /></p>
           </div>
         </card>
       </div>
