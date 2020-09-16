3b843f79d5da478b99e5026030e25795b5caeb32
einobaka <61556425+einobaka@users.noreply.github.com>
2020-09-15 20:46:28 -0700

    added popover for ratings

--- client/src/components/DrinkList/index.js
+++ client/src/components/DrinkList/index.js
@@ -1,13 +1,31 @@
 import React from "react";
 import "./style.css";
 import StaticRating from "../StaticRating"
-
-
+import { OverlayTrigger, Tooltip, Popover, Button } from 'react-bootstrap';
 
 function ListItems(props) {
+
+  // const renderTooltip = (props) => (
+  //   <Tooltip id="button-tooltip" {...props}>
+  //     Simple tooltip
+  //   </Tooltip>);
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Title as="h3">{props.rating} Coffee Stars</Popover.Title>
+      <Popover.Content>
+        {props.review}
+      </Popover.Content>
+    </Popover>
+  );
+
+  const RatingPopover = () => (
+    <OverlayTrigger placement="right" overlay={popover}>
+      <Button variant="primary-outline"><p><StaticRating rating={props.rating} /></p></Button>
+    </OverlayTrigger>
+  );
+
   return (
-
-
 
     <li key={props.name} className="list-group-item">
       <div class="everything">
@@ -23,7 +41,7 @@ function ListItems(props) {
             <p class="price"> <h2> ${props.price} </h2></p>
           </div>
           <div class="drink-ratings">
-            <p><StaticRating rating={props.rating} /></p>
+            <RatingPopover />
           </div>
         </card>
       </div>
