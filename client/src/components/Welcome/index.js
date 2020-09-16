c5d78a8ec61f0d84da68ef2706fd6625b72e9dc8
einobaka <61556425+einobaka@users.noreply.github.com>
2020-09-16 02:12:51 -0700

    updated styling on component

--- client/src/components/Welcome/index.js
+++ client/src/components/Welcome/index.js
@@ -3,18 +3,15 @@ import "./style.css";
 
 
 function WelcomeButton() {
-
-
-    const logout = () => {
-        window.location.href = '/';
-    }
     return (
         <div class='nav-bar item'>
             <p class="welcome">Psst...Hey there, !</p>
-            <p><button onClick={logout} id="logout-btn" class="btn button">LogOut</button></p>
+            <p></p>
         </div>
     )
 }
+
+
 
 
 
