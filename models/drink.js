9f6b086e29bc7c48aef597a18d627e7936804100
Scott Harris <scottsharris@yahoo.com>
2020-09-16 14:25:10 -0700

    drink model cleanup

--- models/drink.js
+++ models/drink.js
@@ -2,11 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const drinkSchema = new Schema({
-  // user: {
-  //   type: Schema.Types.ObjectId,
-  //   ref: "user",
-  //   required: true,
-  // },
   name: { type: String },
   price: { type: String },
   category: { type: String },
